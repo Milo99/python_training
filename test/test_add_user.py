dc2a3ea622dd270c32e485973a593a07335ae933
Kamil <kusiak.kamil@gmail.com>
2018-06-04 21:33:10 +0200

    Session Helper class

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -10,11 +10,11 @@ def app(request):
     return fixture
     
 def test_test_add_user(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(firstname="dfgdghdghdd", middlename="sdgrsgrgsdf", lastname="sdfgsrgsfgsfd",
                         nickname="sdfefsff", title="sd", company="rtgfdgdfggfd", address="dfbdbdfgdfdf",
                         home="3455356356", mobile="24353453456345", work_phone="34563563456", fax="3453456",
                         email="sdfsrfsfs@df.fv", email2="dsfgderfgrdfg@gb.ok", email3="sdfgsdfvgsfv@dfdf.pl",
                         homepage="www.dfgdthgdhgdfhdfgh.ok", birth_year="1980", ann_year="1980",
                         address2="sdfgsrtgdfzvr", phone2="sfdvrgrrfvrf", notes="cvdszfvrgvzrvzrgvfgvzfvrfg"))
-    app.logout()
+    app.session.logout()
