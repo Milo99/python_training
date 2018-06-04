d4766f42f51b0496e8c0078f236334227469f285
Kamil <kusiak.kamil@gmail.com>
2018-06-04 21:17:47 +0200

    Python packages

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 import pytest
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
