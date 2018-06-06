fa6c3298f9fcfbf34661cf240d1eaf46667055be
Kamil <kusiak.kamil@gmail.com>
2018-06-06 10:30:08 +0200

    General fixture for all tests

--- fixture/contact.py
+++ fixture/contact.py
@@ -78,3 +78,26 @@ class ContactHelper(Manager):
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.app.go_back_to_home_page()
+
+    def update_first_contact(self, contact):
+        wd = self.app.wd
+        # init contact update
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        # enter new data in contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        # submit contact edition
+        wd.find_element_by_name("update").click()
+        self.app.go_back_to_home_page()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
---
+++ test/test_update_group.py
@@ -0,0 +1,6 @@
+from model.group import Group
+
+def test_update_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.update_first_group(Group(name="Updated", header="Updated", footer="Updated"))
+    app.session.logout()
