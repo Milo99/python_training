7ea701c0cb38255a16a25f2fe01698120c65d1a9
Kamil <kusiak.kamil@gmail.com>
2018-06-06 09:03:15 +0200

    General fixture for all tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
     
 def test_test_add_user(app):
     app.session.login(username="admin", password="secret")
