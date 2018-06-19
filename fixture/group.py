415fbcf1ca575dcdd2325f358f2c3d9dd8db90fa
Kamil <kusiak.kamil@gmail.com>
2018-06-19 11:21:49 +0200

    List of groups

--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,5 @@
 from fixture.manager import Manager
+from model.group import Group
 
 class GroupHelper(Manager):
 
@@ -63,3 +64,13 @@ class GroupHelper(Manager):
         wd = self.app.wd
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_group_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
