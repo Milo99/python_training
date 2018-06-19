d225e762f780f95a7f377fa5004a0861beba24a9
Kamil <kusiak.kamil@gmail.com>
2018-06-19 13:31:37 +0200

    Group list comparing

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
+
     
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="dfhbgfgbb", header="dfvrtvfdvdf", footer="dfbdgfxdb"))
+    group = Group(name="dfhbgfgbb", header="dfvrtvfdvdf", footer="dfbdgfxdb")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_mas) == sorted(new_groups, key=Group.id_or_mas)
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_mas) == sorted(new_groups, key=Group.id_or_mas)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,15 +3,9 @@ from model.contact import Contact
 def test_modify_firstname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    old_groups = app.group.get_group_list()
     app.contact.modify_first_contact(Contact(firstname="New_first_name"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
 
 def test_modify_lastname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    old_groups = app.group.get_group_list()
     app.contact.modify_first_contact(Contact(firstname="New_lastname_name"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
