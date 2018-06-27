f952a0c0bc92dcfaa23fb0286a69cb672573db5f
Kamil <kusiak.kamil@gmail.com>
2018-06-27 13:25:26 +0200

    DDT for groups and contacts

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+from model.group import Group
+import pytest
+import random
+import string
 
-    
-def test_test_add_user(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Contact(firstname=first, lastname=last)
+    for first in ["", random_string("FIRST", 20)]
+    for last in ("", random_string("LAST", 20))
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_test_add_user(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="fn", lastname="ln")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
