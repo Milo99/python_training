513b4691f1e336b81fd0a1e447038ee19fa79831
Kamil <kusiak.kamil@gmail.com>
2018-07-02 18:05:55 +0200

    Generate & load Groups & Contacts data from Json file

---
+++ data/contacts.py
@@ -0,0 +1,6 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="first1", lastname="last1"),
+    Contact(firstname="first2", lastname="last2")
+]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,25 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from model.group import Group
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-    Contact(firstname=first, lastname=last)
-    for first in ["", random_string("FIRST", 20)]
-    for last in ("", random_string("LAST", 20))
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_test_add_user(app, contact):
+def test_test_add_user(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
